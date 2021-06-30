6a8facad10f1ee76d6b5a71c79ab53a7e0b551df
unknown <henriquebaptista2003@gmail.com>
2021-06-30 17:34:02 -0300

    creating banco class and bacno teste file

---
+++ src/henrique/banco.kt
@@ -0,0 +1,6 @@
+package henrique
+
+data class banco(
+    val nome: String,
+    val numero: Int,
+)
---
+++ src/henrique/teste/BancoTeste.kt
@@ -0,0 +1,12 @@
+package henrique.teste
+
+import henrique.banco
+
+fun main() {
+    val digiOneBank = banco( nome = "DigiOne", numero = 12)
+
+    print(digiOneBank.nome)
+    println(digiOneBank.numero)
+}
+
+
